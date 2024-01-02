c900bf6d1799cd81dbcbfdcfe4658ac7ba4d8e09
Erkenoss <xavier.bertin6@gmail.com>
2024-01-02 14:42:56 +0100

    pycode

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """annote some element here"""
 
-from typing import List ,Iterable, Tuple, Sequence
+from typing import List, Iterable, Tuple, Sequence
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Annote some element"""
     return [(i, len(i)) for i in lst]
