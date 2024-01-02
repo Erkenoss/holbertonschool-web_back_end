b0f6f625763c69c6b8abc648ed5c230637215249
Erkenoss <xavier.bertin6@gmail.com>
2024-01-02 11:13:04 +0100

    fixe typing ist

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """ sixth basic annotation """
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+
+def sum_list(input_list: List[float] ) -> float:
     """return the sum of number in a list"""
     sum: float = 0.0
     for element in input_list:
         sum += element
-    return sum
+    return float(sum)
