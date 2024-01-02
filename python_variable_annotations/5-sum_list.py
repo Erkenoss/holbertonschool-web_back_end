6559202a96ef4523e8c01309f1da90d9303d78fa
Erkenoss <xavier.bertin6@gmail.com>
2024-01-02 11:13:53 +0100

    pycode

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,7 +4,7 @@
 from typing import List
 
 
-def sum_list(input_list: List[float] ) -> float:
+def sum_list(input_list: List[float]) -> float:
     """return the sum of number in a list"""
     sum: float = 0.0
     for element in input_list:
