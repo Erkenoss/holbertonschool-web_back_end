bdfb6e8ca0b67daecd0affa2a2219a4a75a908ee
Erkenoss <xavier.bertin6@gmail.com>
2024-01-02 12:47:54 +0100

    add task 7-

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, int or float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """tuple of two element"""
-    return k, (v * v)
+    return k, v * v
