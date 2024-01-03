1f0db8bcc8bcb815afbe58fd356d73463ed87e0d
Erkenoss <xavier.bertin6@gmail.com>
2024-01-03 16:34:43 +0100

    idk what happend

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""asynchronous coroutine"""
+""" second async function """
 import asyncio
 from typing import List
 
@@ -8,10 +8,13 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Create a liste to launch n time wait  random and return a
-    newlist sorted"""
-    list_delay = []
-    for _ in range(n):
-        list_delay.append(wait_random(max_delay))
-    result = await asyncio.gather(*list_delay)
-    return sorted(result)
+    """create a list of random"""
+    list_r = []
+    for element in range(n):
+        list_r.append(wait_random(max_delay))
+        # element = wait_random(max_delay)
+        # list_r.append(element)
+
+    List_sort = await asyncio.gather(*list_r)
+
+    return sorted(List_sort)
