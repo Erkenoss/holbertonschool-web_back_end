dfcc7e74db4d0d96c9cb75336a16e1ec3b90b6a1
Erkenoss <xavier.bertin6@gmail.com>
2024-01-03 15:49:04 +0100

    add task 0-

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" first async function """
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    random_value = random.random() * max_delay
+    await asyncio.sleep(random_value)
+    return random_value
