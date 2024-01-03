de3c13add14e03a320305939cbf0289b94cab229
Erkenoss <xavier.bertin6@gmail.com>
2024-01-03 16:38:08 +0100

    idk what happend

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """create a list of random"""
-    if n < 1: 
+    if n < 1:
         return
     list_r = []
     for element in range(n):
