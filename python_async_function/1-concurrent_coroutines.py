46839658581e63b39236c51d3df80e2009102d0e
Erkenoss <xavier.bertin6@gmail.com>
2024-01-03 16:37:14 +0100

    idk what happend

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """create a list of random"""
     if n < 1: 
-        return []
+        return
     list_r = []
     for element in range(n):
         list_r.append(wait_random(max_delay))
