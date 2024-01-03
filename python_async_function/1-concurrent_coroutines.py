afd33739bd749e61e90ea03ca00f3fbedb7ab309
Erkenoss <xavier.bertin6@gmail.com>
2024-01-03 16:16:53 +0100

    add task 1-

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" second async function """
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """create a list of random"""
+    list_r = []
+    for element in range(n):
+        element = wait_random(max_delay)
+        list_r.append(element)
+
+    List_sort = await asyncio.gather(*list_r)
+
+    return sorted(List_sort)
