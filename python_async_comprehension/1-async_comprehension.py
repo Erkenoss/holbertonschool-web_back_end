8a0c1fa8744de268e2e04f2444ea1a73755e8235
Erkenoss <xavier.bertin6@gmail.com>
2024-01-04 11:24:43 +0100

    fixed task 1-

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""  """
+""" use async for """
 import asyncio
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
+    """ return a list of number random generate """
     rand_x_ten = [numb async for numb in async_generator()]
     return rand_x_ten
