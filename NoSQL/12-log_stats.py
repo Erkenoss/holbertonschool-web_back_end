a863adef3d9e59e9c523d15fea44a0077976935a
Erkenoss <xavier.bertin6@gmail.com>
2024-01-08 16:14:40 +0100

    add task 12-

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+def nginx_logs_stats(mongo_collection):
+    try:
+        total_logs = mongo_collection.count_documents({})
+        print(f"Total logs: {total_logs}")
+
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        print("Methods:")
+        for method in methods:
+            count = mongo_collection.count_documents({"method": method})
+            print(f"\t{method}: {count}")
+
+        special_logs_count = mongo_collection.count_documents({"method": "GET", "path": "/status"})
+        print(f"GET requests to /status: {special_logs_count}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017/')
+
+    db = client.logs
+    collection = db.nginx
+
+    nginx_logs_stats(collection)
+
+    client.close()
