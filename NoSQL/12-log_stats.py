d19f6f697257a161a7e39a180db5b67491082346
Erkenoss <xavier.bertin6@gmail.com>
2024-01-08 16:17:58 +0100

    add task 12-

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
-""" script that provides some stats about Nginx logs stored in MongoDB """
+"""
+This is the 12-log_stats module
+"""
+
 from pymongo import MongoClient
 
 
-def nginx_logs_stats(mongo_collection):
-    """ script that provides some stats about Nginx logs stored in MongoDB """
-    try:
-        total_logs = mongo_collection.count_documents({})
-        print(f"Total logs: {total_logs}")
+def log_stats(logs_collection):
+    """
+    Provides some formatted stats about Nginx logs stored in MongoDB.
+    """
 
-        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-        print("Methods:")
-        for method in methods:
-            count = mongo_collection.count_documents({"method": method})
-            print(f"\t{method}: {count}")
+    total_logs = logs_collection.count_documents({})
+    print(f"{total_logs} logs")
 
-        special_logs_count = mongo_collection.count_documents({"method": "GET", "path": "/status"})
-        print(f"GET requests to /status: {special_logs_count}")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = logs_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    status_check_count = logs_collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print(f"{status_check_count} status check")
+
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017/')
-
-    db = client.logs
-    collection = db.nginx
-
-    nginx_logs_stats(collection)
-
-    client.close()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    log_stats(logs_collection)
