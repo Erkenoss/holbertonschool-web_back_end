36a28c657688faa76cb7bff05cd9c97830bc5a15
Erkenoss <xavier.bertin6@gmail.com>
2024-01-09 15:43:41 +0100

    pycode

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,5 +7,9 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> tuple:
+    """
+    return tuple (index 0 of page given and
+    the number of element at the end of the page)
+    """
     index = page - 1
     return index * page_size, page_size * page
