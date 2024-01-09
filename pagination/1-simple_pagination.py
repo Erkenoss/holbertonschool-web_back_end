b33dfcfd859758f01e667de77e733071f9930d37
Erkenoss <xavier.bertin6@gmail.com>
2024-01-09 17:14:34 +0100

    add task 1-*

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -33,6 +33,7 @@ class Server:
         return index * page_size, page_size * page
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ return list on the index given """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
