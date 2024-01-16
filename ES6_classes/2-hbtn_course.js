9b777b7d49fbbb4406e3efda2338ce0bd1925bc0
Erkenoss <xavier.bertin6@gmail.com>
2024-01-16 10:00:30 +0100

    add task 2-

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -19,22 +19,28 @@ export default class HolbertonCourse {
 
   set name(newName) {
     if (typeof newName === 'string') {
-      this._name = newName; }
+      this._name = newName;
+    }
     else {
-      throw new TypeError('Name must be a string'); }
+      throw new TypeError('Name must be a string');
+    }
   }
 
   set length(newLength) {
     if (typeof newLength === 'number') {
-        this._length = newLength; }
+      this._length = newLength;
+      }
     else {
-        throw new TypeError('Length must be a number'); }
+      throw new TypeError('Length must be a number');
+    }
   }
 
   set students(newStudents) {
     if (Array.isArray(newStudents) && newStudents.every(student => typeof student === 'string')) {
-      this._students = newStudents; }
+      this._students = newStudents;
+    }
     else {
-      throw new TypeError('Students must be an array of strings'); }
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 }
