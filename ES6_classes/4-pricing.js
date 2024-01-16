38a0d6346c9afa32cdc52d1324964fd5f2abd785
Erkenoss <xavier.bertin6@gmail.com>
2024-01-16 12:06:16 +0100

    add task 4-

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,47 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = Pricing.valnumber(amount, 'Amount');
+    this._currency = Pricing.classtest(currency, Currency, 'Currency');
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(newAmount) {
+    this._amount = Pricing.valnumber(newAmount, 'Amount');
+  }
+
+  set currency(newCurrency) {
+    this._currency = Pricing.classtest(newCurrency, Currency, 'Currency');
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, convertRate) {
+    return amount * convertRate;
+  }
+
+  static valnumber(value, propertyName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${propertyName} Must be a number`)
+    }
+    return value;
+  }
+
+  static classtest(value, classType, propertyName) {
+    if (value instanceof classType) {
+      return value;
+    } else {
+    throw new TypeError(`${classType.name} expected for ${propertyName}`);
+    }
+  }
+}
