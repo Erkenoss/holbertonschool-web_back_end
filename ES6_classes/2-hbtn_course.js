a04f79598963384cceaafde9fd88cbfa31863c25
Erkenoss <xavier.bertin6@gmail.com>
2024-01-16 10:02:57 +0100

    eslint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -20,8 +20,7 @@ export default class HolbertonCourse {
   set name(newName) {
     if (typeof newName === 'string') {
       this._name = newName;
-    }
-    else {
+    } else {
       throw new TypeError('Name must be a string');
     }
   }
@@ -29,8 +28,7 @@ export default class HolbertonCourse {
   set length(newLength) {
     if (typeof newLength === 'number') {
       this._length = newLength;
-      }
-    else {
+    } else {
       throw new TypeError('Length must be a number');
     }
   }
@@ -38,8 +36,7 @@ export default class HolbertonCourse {
   set students(newStudents) {
     if (Array.isArray(newStudents) && newStudents.every(student => typeof student === 'string')) {
       this._students = newStudents;
-    }
-    else {
+    } else {
       throw new TypeError('Students must be an array of strings');
     }
   }
