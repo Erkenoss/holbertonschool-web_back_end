06ceea6407fec4e87719f698ff0128767dfe8d45
Erkenoss <xavier.bertin6@gmail.com>
2024-01-16 11:01:31 +0100

    change all code

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,43 +1,52 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this._name = HolbertonCourse.valString(name, 'Name');
+    this._length = HolbertonCourse.valNumber(length, 'Length');
+    this._students = HolbertonCourse.valArray(students, 'Students');
   }
 
   get name() {
     return this._name;
   }
 
+  set name(newName) {
+    this._name = HolbertonCourse.valString(newName, 'Name');
+  }
+
   get length() {
     return this._length;
+  }
+
+  set length(newLength) {
+    this._length = HolbertonCourse.valNumber(newLength, 'Length');
   }
 
   get students() {
     return this._students;
   }
 
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
-  }
-
-  set length(newLength) {
-    if (typeof newLength === 'number') {
-      this._length = newLength;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
-  }
-
   set students(newStudents) {
-    if (Array.isArray(newStudents) && newStudents.every((student) => typeof student === 'string')) {
-      this._students = newStudents;
-    } else {
-      throw new TypeError('Students must be an array of strings');
+    this._students = HolbertonCourse.valArray(newStudents, 'Students');
+  }
+
+  static valString(value, propertyName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${propertyName} Must be a string`);
     }
+    return value;
+  }
+
+  static valNumber(value, propertyName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${propertyName} Must be a number`);
+    }
+    return value;
+  }
+
+  static valArray(value, propertyName) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${propertyName} Must be an array`);
+    }
+    return value;
   }
 }
