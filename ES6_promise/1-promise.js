c97189ba13a18ad20856b76a158d24bf9506615a
Erkenoss <xavier.bertin6@gmail.com>
2024-01-22 20:03:14 +0100

    eslint

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,11 +1,9 @@
-import { promises } from "dns";
-
 export default function getFullResponseFromAPI(success) {
   const myPromise = new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: success });
     } else {
-      reject('The fake API is not working currently')
+      reject(new Error('The fake API is not working currently'));
     }
   });
   return myPromise;
