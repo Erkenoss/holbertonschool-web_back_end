f9837430fed505372275b917051673b8d2b7c3a3
Erkenoss <xavier.bertin6@gmail.com>
2024-01-26 11:08:56 +0100

    fixed reject

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,6 +1,5 @@
-export default function uploadPhoto(filename) {
-  const myPromise = new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  });
-  return myPromise;
+#!/usr/bin/node
+
+export default function uploadPhoto(fileName) {
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
