b88bae75f65d99c20ac3291d72e4c0aea0cc6d55
Erkenoss <xavier.bertin6@gmail.com>
2024-01-26 11:11:24 +0100

    fixed task 6-

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,12 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
-    .then(([signUpResult, uploadResult]) => [[{ ...signUpResult }, { ...uploadResult }]])
-    .catch((error) => {
-      throw error;
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const promises = [];
+      results.forEach((promise) => {
+        promises.push({
+          status: promise.status,
+          value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+        });
+      });
+      return promises;
     });
 }
