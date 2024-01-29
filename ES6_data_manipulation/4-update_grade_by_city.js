bb2f17e1fa61d772c8103a7eeb0528497f103f82
Erkenoss <xavier.bertin6@gmail.com>
2024-01-29 11:29:04 +0100

    add task 4-

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(getListStudents, city, newGrades)
+{
+  const studentsInCity = getListStudents.filter(student => student.location.includes(city));
+
+  const updatedStudents = studentsInCity.map(student => {
+    const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
+
+    if (matchingGrade) {
+      return { ...student, grade: matchingGrade.grade };
+    } else {
+      return { ...student, grade: 'N/A' };
+    }
+  });
+
+  return updatedStudents;
+}
