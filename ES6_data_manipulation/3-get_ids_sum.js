1b4caa9b5f42434451882a392f5a01d64979fa59
Erkenoss <xavier.bertin6@gmail.com>
2024-01-29 11:09:10 +0100

    fixed task 3-

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(getListStudents)
 {
-  return getListStudents.reduce((acc, array) => acc + array, 0);
+  return getListStudents.reduce((acc, array) => acc + array.id, 0);
 }
