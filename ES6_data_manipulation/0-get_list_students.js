59fc5db82cc0e9f40032caff1d300929db24a96d
Erkenoss <xavier.bertin6@gmail.com>
2024-01-29 10:29:00 +0100

    add task 0-

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const students = [
+      { id: 1, firstName: 'Guillaume', location: 'in San Francisco' },
+      { id: 2, firstName: 'James', location: 'in Columbia' },
+      { id: 5, firstName: 'Serena', location: 'in San Francisco' }
+  ];
+
+  return students;
+}
