b93c2c5d60b51fd0603d57e78c39a2f9f8e1aa0e
Erkenoss <xavier.bertin6@gmail.com>
2024-01-30 09:43:43 +0100

    eslint 4-

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,13 @@
-export default function updateStudentGradeByCity(getListStudents, city, newGrades)
-{
-  const studentsInCity = getListStudents.filter(student => student.location.includes(city));
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  const studentsInCity = getListStudents.filter((student) => student.location.includes(city));
 
-  const updatedStudents = studentsInCity.map(student => {
-    const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
+  const updatedStudents = studentsInCity.map((student) => {
+    const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
 
     if (matchingGrade) {
       return { ...student, grade: matchingGrade.grade };
-    } else {
-      return { ...student, grade: 'N/A' };
     }
+    return { ...student, grade: 'N/A' };
   });
 
   return updatedStudents;
