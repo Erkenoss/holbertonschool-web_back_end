720f9cea9a70c683effbb0114838f1f1f47ef2bc
Erkenoss <xavier.bertin6@gmail.com>
2024-01-30 09:49:10 +0100

    fixed 5-?

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value)
 {
   if (length < 0 || position >= length) {
-    throw new Error('Position outside of range');
+    throw new Error('Position outside range');
   }
 
   let buffer = new ArrayBuffer(length);
