56b11877acf04d6806047a40b166ebcba31cd50c
Erkenoss <xavier.bertin6@gmail.com>
2024-01-30 09:59:09 +0100

    fixed 10-

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -3,11 +3,9 @@ export default function updateUniqueItems(inputMap) {
     throw new Error('Cannot process');
   }
 
-  const updatedMap = new Map();
-
   inputMap.forEach((quantity, item) => {
-    updatedMap.set(item, quantity === 1 ? 100 : quantity);
+    inputMap.set(item, quantity === 1 ? 100 : quantity);
   });
 
-  return updatedMap;
+  return inputMap;
 }
